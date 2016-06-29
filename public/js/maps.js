a1d74401c96e7a005854a9af018e123b14a113a9
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-29 17:31:26 -0400

    dangyu sucks

---
+++ public/js/maps.js
@@ -0,0 +1,29 @@
+function initialize_gmaps(){
+  //initialize new google maps LatLng object
+  var myLatlng = new google.maps.LatLng(40.705189, -74.009209);
+
+  //set the map options hash
+  var mapOptions = {
+    center: myLatlng,
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  //get the map div's HTML obj
+  var map_canvas_obj = document.getElementById("map-canvas");
+  //initialize a new Google Map with the otpions
+  var map = new google.maps.Map(map_canvas_obj, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    title: "Hello Dangyu!"
+  });
+
+  //add the marker to the map by calling setMap()
+  marker.setMap(map);
+
+}
+
+$(document).ready(function(){
+  initialize_gmaps();
+})
